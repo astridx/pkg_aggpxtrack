115aa3dee86764e003915958abe6616abd03347b
astridx <info@astrid-guenther.de>
2020-02-23 12:10:54 +0100

    wip

--- src/administrator/manifests/packages/aggpxtrack/script.php
+++ src/administrator/manifests/packages/aggpxtrack/script.php
@@ -27,4 +27,47 @@ class Pkg_AggpxtrackInstallerScript extends JInstallerScript
 		$this->minimumJoomla = '4.0.0-beta1-dev';
 		$this->minimumPhp    = JOOMLA_MINIMUM_PHP;
 	}
+
+	/**
+	 * Method to run after the install routine.
+	 *
+	 * @param   string                      $type    The action being performed
+	 * @param   JInstallerAdapterComponent  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.76
+	 */
+	public function postflight($type, $parent)
+	{
+		return $this->copyFiles();
+	}
+
+	/**
+	 * Method to rcopy files.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.0.76
+	 */
+	public function copyFiles()
+	{
+		$mode = 0755;
+		$path = JPATH_SITE . "/images/com_aggpxtrack/";
+		JFolder::create($path, $mode);
+
+		$pathsearch = JPATH_SITE . "/media/plg_fields_aggpxtrack/gpxfiles/";
+
+		$gpx_files = JFolder::files($pathsearch, '.gpx');
+
+		foreach ($gpx_files as $file)
+		{
+			if (!file_exists($path . $file))
+			{
+				JFile::copy($pathsearch . $file, $path . $file);
+			}
+		}
+
+		return true;
+	}
 }
