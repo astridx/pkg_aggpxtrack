9fb73086badbe157e015ff4adf6494346e9d7658
astridx <info@astrid-guenther.de>
2020-10-04 10:10:17 +0200

    wip

--- src/administrator/manifests/packages/aggpxtrack/script.php
+++ src/administrator/manifests/packages/aggpxtrack/script.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  pkg_aggpxtrack
  *
- * @copyright   Copyright (C) 2005 - 2018 Astrid Günther, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2020 Astrid Günther, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later;
  * @link        astrid-guenther.de
  */
